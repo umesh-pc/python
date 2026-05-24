77d63b67245bde2d7e5d74ad3d884c628601d703
umesh-pc <umeshskannan@gmail.com>
2026-05-24 11:06:56 +0530

    positive,negative,zero

---
+++ divisible_2.py
@@ -0,0 +1,2 @@
+n=int(input("enter a number "))
+print("divisible by 2 "if n%2==0 else "not divisible by 2")
---
+++ divisible_3.py
@@ -0,0 +1,2 @@
+n=int(input("enter a number "))
+print("divisible by 3" if n%3==0 else "not divisible by 3")
