57453f0e3fa3b44b0d09566f1b08be3cbb0deb51
umesh-pc <umeshskannan@gmail.com>
2026-05-24 11:13:22 +0530

    divisible by 5

--- divisible_3.py
+++ divisible_3.py
@@ -1,2 +1,2 @@
-n=int(input("enter a number "))
+n=int(input("enter a number :"))
 print("divisible by 3" if n%3==0 else "not divisible by 3")
---
+++ divisible_5.py
@@ -0,0 +1,2 @@
+n=int (input("enter a number : "))
+print("divisible by 5" if n%5==0 else "not divisible by 5")
