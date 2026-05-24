fe2f6c943435e6bfe76c34ecb424c4b76b2ed7c7
umesh-pc <umeshskannan@gmail.com>
2026-05-24 08:33:33 +0530

    positive,negative,zero

---
+++ positive_negative_zero.py
@@ -0,0 +1,8 @@
+a=int(input("enter a number "))
+if a>0:
+    print("the number ",a,"is positive")
+elif a<0:
+    print("the number ",a,"is negative")
+else:
+    print("the number is zero")
+           
