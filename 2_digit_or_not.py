8ce2b33fe8eed809c5a9c0b3bb2e0ec1fbcc8f14
umesh-pc <umeshskannan@gmail.com>
2026-05-26 07:59:11 +0530

    checking no of digits

---
+++ 2_digit_or_not.py
@@ -0,0 +1,5 @@
+n=input("Enter a number: ")
+if len(n)==2:
+    print("the number is a two-digit number")
+else:
+    print("the number is not a two-digit number it is ", len(n), "digit number")
---
+++ 3_digit_or_not.py
@@ -0,0 +1,5 @@
+n=input("Enter a number: ")
+if len(n)==3:
+    print("the number is a three-digit number")
+else:
+    print("the number is not a three-digit number it is ", len(n), "digit number")
