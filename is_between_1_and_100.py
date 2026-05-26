87a91d10b81cd5d0ca039de9eaf63bc55bc10bfd
umesh-pc <umeshskannan@gmail.com>
2026-05-26 08:08:24 +0530

    c

---
+++ is_between_1_and_100.py
@@ -0,0 +1,5 @@
+n=int(input("enter a number "))
+if n>1 and n<100:
+    print ("the number is between 1 and 100")
+else:
+    print("the number is not between 1 and 100")
---
+++ multiple_of_7.py
@@ -0,0 +1,2 @@
+n=int(input("enter a numbe "))
+print("the number is multiple of 7", "yes"if n%7==0 else "no")
