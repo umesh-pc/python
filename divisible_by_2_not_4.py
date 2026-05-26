2cc8d4ba2f16aea334030fff1dd9395eccd50acf
umesh-pc <umeshskannan@gmail.com>
2026-05-26 08:57:26 +0530

    divisible test

---
+++ divisible_by_2_not_4.py
@@ -0,0 +1,2 @@
+n=int(input("enter a number: "))
+print("the number is divisible by 2 but not by 4", "yes" if n%2==0 and n%4!=0 else "no")
---
+++ divisible_by_4_or_6.py
@@ -0,0 +1,2 @@
+n=int(input("enetr a number "))
+print("the number is divisible by 4 or 6","yes" if n%4==0 or n%6==0 else "no")
