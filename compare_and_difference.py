0939e66530cb93fe2ec24f13a16e34cd04025b52
umesh-pc <umeshskannan@gmail.com>
2026-05-27 08:52:09 +0530

    compare

---
+++ compare_and_difference.py
@@ -0,0 +1,8 @@
+a=int(input("enter a number"))
+b=int(input("enter another number"))
+if a>b:
+    print("the larger number is",a, "difference between them is", a-b)
+elif b>a:
+    print("the larger number is",b, "difference between them is", b-a )
+else:
+    print("both numbers are equal")
